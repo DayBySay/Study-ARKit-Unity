38b8d73a378b127b539ee6c6373f195f6cf3be68
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-10-03 22:48:48 +0900

    ...

--- Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARCameraManager.cs
+++ Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARCameraManager.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.iOS;
 
-public class UnityARCameraManager : MonoBehaviour {
+public class UnityARCameraManager : MonoBehaviour
+{
 
-    public Camera m_camera;
-    private UnityARSessionNativeInterface m_session;
+	public Camera m_camera;
+	private UnityARSessionNativeInterface m_session;
 	private Material savedClearMaterial;
 
-	[Header("AR Config Options")]
+	[Header ("AR Config Options")]
 	public UnityARAlignment startAlignment = UnityARAlignment.UnityARAlignmentGravity;
 	public UnityARPlaneDetection planeDetection = UnityARPlaneDetection.Horizontal;
 	public bool getPointCloud = true;
 	public bool enableLightEstimation = true;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 
-		m_session = UnityARSessionNativeInterface.GetARSessionNativeInterface();
+		m_session = UnityARSessionNativeInterface.GetARSessionNativeInterface ();
 
 #if !UNITY_EDITOR
 		Application.targetFrameRate = 60;
@@ -37,14 +39,14 @@ public class UnityARCameraManager : MonoBehaviour {
 		UnityARCamera scamera = new UnityARCamera ();
 		scamera.worldTransform = new UnityARMatrix4x4 (new Vector4 (1, 0, 0, 0), new Vector4 (0, 1, 0, 0), new Vector4 (0, 0, 1, 0), new Vector4 (0, 0, 0, 1));
 		Matrix4x4 projMat = Matrix4x4.Perspective (60.0f, 1.33f, 0.1f, 30.0f);
-		scamera.projectionMatrix = new UnityARMatrix4x4 (projMat.GetColumn(0),projMat.GetColumn(1),projMat.GetColumn(2),projMat.GetColumn(3));
+		scamera.projectionMatrix = new UnityARMatrix4x4 (projMat.GetColumn (0), projMat.GetColumn (1), projMat.GetColumn (2), projMat.GetColumn (3));
 
 		UnityARSessionNativeInterface.SetStaticCamera (scamera);
 
 #endif
 	}
 
-	public void SetCamera(Camera newCamera)
+	public void SetCamera (Camera newCamera)
 	{
 		if (m_camera != null) {
 			UnityARVideo oldARVideo = m_camera.gameObject.GetComponent<UnityARVideo> ();
@@ -56,34 +58,34 @@ public class UnityARCameraManager : MonoBehaviour {
 		SetupNewCamera (newCamera);
 	}
 
-	private void SetupNewCamera(Camera newCamera)
+	private void SetupNewCamera (Camera newCamera)
 	{
 		m_camera = newCamera;
 
-        if (m_camera != null) {
-            UnityARVideo unityARVideo = m_camera.gameObject.GetComponent<UnityARVideo> ();
-            if (unityARVideo != null) {
-                savedClearMaterial = unityARVideo.m_ClearMaterial;
-                Destroy (unityARVideo);
-            }
-            unityARVideo = m_camera.gameObject.AddComponent<UnityARVideo> ();
-            unityARVideo.m_ClearMaterial = savedClearMaterial;
-        }
+		if (m_camera != null) {
+			UnityARVideo unityARVideo = m_camera.gameObject.GetComponent<UnityARVideo> ();
+			if (unityARVideo != null) {
+				savedClearMaterial = unityARVideo.m_ClearMaterial;
+				Destroy (unityARVideo);
+			}
+			unityARVideo = m_camera.gameObject.AddComponent<UnityARVideo> ();
+			unityARVideo.m_ClearMaterial = savedClearMaterial;
+		}
 	}
 
 	// Update is called once per frame
 
-	void Update () {
+	void Update ()
+	{
 		
-        if (m_camera != null)
-        {
-            // JUST WORKS!
-            Matrix4x4 matrix = m_session.GetCameraPose();
-			m_camera.transform.localPosition = UnityARMatrixOps.GetPosition(matrix);
+		if (m_camera != null) {
+			// JUST WORKS!
+			Matrix4x4 matrix = m_session.GetCameraPose ();
+			m_camera.transform.localPosition = UnityARMatrixOps.GetPosition (matrix);
 			m_camera.transform.localRotation = UnityARMatrixOps.GetRotation (matrix);
 
-            m_camera.projectionMatrix = m_session.GetCameraProjection ();
-        }
+			m_camera.projectionMatrix = m_session.GetCameraProjection ();
+		}
 
 	}
 
